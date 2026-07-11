1099e46215241eee53882669a35974fabc6f62f1
hamedhomaee <hamedhomaee71@gmail.com>
2026-07-11 15:42:00 +0200

    project-1-us-9 - Column names added to Employee entity configuration

--- Company Connect/CompanyConnect/CompanyConnect.Infrastructure/EntityTypeConfigurations/EmployeeEntityTypeConfiguration.cs
+++ Company Connect/CompanyConnect/CompanyConnect.Infrastructure/EntityTypeConfigurations/EmployeeEntityTypeConfiguration.cs
@@ -9,49 +9,65 @@ public class EmployeeEntityTypeConfiguration : IEntityTypeConfiguration<Employee
     public void Configure(EntityTypeBuilder<Employee> builder)
     {
         builder
+            .ToTable("Employees");
+
+        builder
             .HasKey(e => e.Id);
+        builder
+            .Property(e =>e.Id)
+            .HasColumnName("Id");
         
         builder
             .Property(e => e.FirstName)
-            .HasMaxLength(50);
+            .HasMaxLength(50)
+            .HasColumnName("FirstName");
 
         builder
             .Property(e => e.LastName)
-            .HasMaxLength(50);
+            .HasMaxLength(50)
+            .HasColumnName("LastName");
 
         builder
             .Property(e => e.Email)
-            .HasMaxLength(50);
+            .HasMaxLength(50)
+            .HasColumnName("Email");
         builder
             .HasIndex(e => e.Email)
             .IsUnique();
 
         builder
             .Property(e => e.PhoneNumber)
-            .HasMaxLength(20);
+            .HasMaxLength(20)
+            .HasColumnName("PhoneNumber");
 
         builder
             .Property(e => e.HireDate)
-            .HasColumnType("date");
+            .HasColumnType("date")
+            .HasColumnName("HireDate");
 
         builder
             .Property(e => e.JobTitle)
-            .HasMaxLength(100);
+            .HasMaxLength(100)
+            .HasColumnName("JobTitle");
         
         builder
             .Property(e => e.Salary)
-            .HasColumnType("decimal");
+            .HasColumnType("decimal")
+            .HasColumnName("Salary");
 
         builder
             .Property(e => e.IsEmploymentTerminated)
-            .HasColumnType("bit");
+            .HasColumnType("bit")
+            .HasColumnName("IsEmploymentTerminated");
 
         builder
             .Property(e => e.CreatedAt)
-            .HasColumnType("datetime2");
+            .HasColumnType("datetime2")
+            .HasColumnName("CreatedAt");
 
         builder
             .Property(e => e.UpdatedAt)
-            .HasColumnType("datetime2");
+            .HasColumnType("datetime2")
+            .HasColumnName("UpdatedAt");
     }
 }
